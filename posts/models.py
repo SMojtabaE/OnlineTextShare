5f473ea7454af54ad5753e5a68f3d18c18363761
Seyed Mojtaba <mo93esm@gmail.com>
2024-04-25 15:17:24 +0330

    home view added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['date_posted']
+
+        
+    def __str__(self):
+        return self.title
