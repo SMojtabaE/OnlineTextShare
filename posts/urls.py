19d39fee06d1512c335ff4fdd033079fd43489ca
Seyed Mojtaba <mo93esm@gmail.com>
2024-04-27 13:37:22 +0330

    post detail and delete added,plus some css

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import (HomeView,PostCreateview,PostsListView)
+from .views import (HomeView,PostCreateview,PostsListView,
+                    PostDetailview,PostUpdateView,PostDeleteView)
 app_name = 'posts'
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
     path('create_post/',PostCreateview.as_view(),name='create_post'),
     path('list_posts/',PostsListView.as_view(),name='list_posts'),
+    path('post_detail/<int:pk>',PostDetailview.as_view(),name='detail_post'),
+    path('update_post/<int:pk>',PostUpdateView.as_view(),name='post_update'),
+    path('delete_post/<int:pk>',PostDeleteView.as_view(),name='delete_post')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,CreateView,ListView
+from django.views.generic import TemplateView,CreateView,ListView,DetailView,UpdateView,DeleteView
 from .models import Post
 from django.urls import reverse_lazy
 # Create your views here.
@@ -17,3 +17,16 @@ class PostsListView(ListView):              # template = > post_list.html
     model = Post
 
     context_object_name = 'posts_list'
+
+class PostDetailview(DetailView):           # tempalte = > post_detail.html
+    model = Post
+
+
+class PostUpdateView(UpdateView):           # template = > post_form.html    build befor
+    model = Post
+    fields = ['title', 'content']
+    success_url = reverse_lazy('posts:list_posts')
+
+class PostDeleteView(DeleteView):           # template = > post_confirm_delete.html
+    model = Post
+    success_url = reverse_lazy('posts:list_posts')
