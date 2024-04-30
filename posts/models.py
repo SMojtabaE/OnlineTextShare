8c9c6518830a2151d57fd603c51f1ce3d09d14a0
Seyed Mojtaba <mo93esm@gmail.com>
2024-04-30 15:15:59 +0330

    added user field to post model

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
+    
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    # user = models.ForeignKey(User,on_delete=models.CASCADE)
-
+    is_publick = models.BooleanField(default=False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    
     class Meta:
         ordering = ['date_posted']
 
